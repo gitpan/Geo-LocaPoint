c00e2bfee52a02f9c350f7713db86c79cd066215
OHTSUKA Ko-hei <nene@kokogiko.net>
2009-03-24 08:35:41 +0000

    Import of KOKOGIKO/Geo-LocaPoint-0.0.4 from CPAN.
    gitpan-cpan-distribution: Geo-LocaPoint
    gitpan-cpan-version:      0.0.4
    gitpan-cpan-path:         KOKOGIKO/Geo-LocaPoint-0.0.4.tar.gz
    gitpan-cpan-author:       KOKOGIKO
    gitpan-cpan-maturity:     released

--- lib/Geo/LocaPoint.pm
+++ lib/Geo/LocaPoint.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use Carp;
 
-use version; our $VERSION = qv('0.0.3');
+use version; our $VERSION = qv('0.0.4');
 use vars qw(@ISA @EXPORT @EXPORT_OK);
 use Exporter;
 @ISA = qw(Exporter);
